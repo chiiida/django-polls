5f30eafde20c180d2e5d5d66155ddffec0d5d329
Chananchida F <chida.fua@gmail.com>
2019-11-04 16:23:35 +0700

    add signup page

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-
-import mysite.views
+from . import views
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-    path('', mysite.views.index),
+    path('', views.index),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,9 +1,25 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login
 
 
 def index(request):
     """Redirects to polls index page."""
     return HttpResponseRedirect(reverse('polls:index'))
-    
+
+# def signup(request):
+#     """Register a new user."""
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             raw_passwd = form.cleaned_data.get('password')
+#             user = authenticate(username=username,password=raw_passwd)
+#             login(request, user)
+#             return redirect('polls')
+#         # what if form is not valid?
+#         # we should display a message in signup.html
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'registration/signup.html', {'form': form})
